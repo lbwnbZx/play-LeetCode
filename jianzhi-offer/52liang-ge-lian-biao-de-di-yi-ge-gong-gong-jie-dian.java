c3945b78a99d1bbe92f28bbd0787567e9238ff63
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-04-28 11:38:37 +0800

    两个链表的第一个公共结点

---
+++ jianzhi-offer/52liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ //˫ָ�뷨
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pa = headA , pb = headB;
+        while(pa!=pb){
+            pa = pa == null ? pa = headB : pa.next;
+            pb = pb == null ? pb = headA : pb.next; 
+        }
+        return pa;
+    }
+}

832b1dcd4ad356c24bf626333daf4899f3fbc6a6
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-04-29 12:55:53 +0800

    平衡二叉树

---
+++ jianzhi-offer/57ping-heng-er-cha-shu.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+    public int recur(TreeNode root){
+        if(root == null) return 0;
+        int left = recur(root.left);
+        if(left == -1) return -1;
+        int right=recur(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+}

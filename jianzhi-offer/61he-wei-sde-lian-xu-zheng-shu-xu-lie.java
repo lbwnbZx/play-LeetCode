06a9340d370da71c313dcb4cb1ef37767dfbca2d
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-05-03 00:16:12 +0800

    和为s的连续正数序列

---
+++ jianzhi-offer/61he-wei-sde-lian-xu-zheng-shu-xu-lie.java
@@ -0,0 +1,30 @@
+﻿class Solution {
+    public int[][] findContinuousSequence(int target) {
+        int i = 1;
+        int j = 1;
+        int sum = 0;
+        List<int []> res =  new ArrayList<>();
+
+        while(i <= target/2){
+            if(sum < target){
+                //右边界右移
+                sum += j;
+                j++;
+            }else if(sum > target){
+                //左边界右移
+                sum -= i;
+                i++;
+            }else{
+                int [] arr = new int[j-i];
+                for(int k=i;k<j;k++){
+                    arr[k-i]=k;
+                }
+                res.add(arr);
+                //左边界向右移动
+                sum -= i;
+                i++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+     }
+}

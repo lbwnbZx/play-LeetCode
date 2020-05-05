c746138e2dd33b35a7be6faf4bb763415c997aac
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-05-05 10:31:37 +0800

    翻转单词顺序

---
+++ jianzhi-offer/62fan-zhuan-dan-ci-shun-xu.java
@@ -0,0 +1,14 @@
+﻿class Solution {
+    public String reverseWords(String s) {
+        s=s.trim();
+        int i=s.length()-1,j=i;
+        StringBuilder res = new StringBuilder();
+        while(i>=0){
+            while(i>=0 && s.charAt(i)!=' ') i--;//搜索单词
+            res.append(s.substring(i+1,j+1)+' ');
+            while(i>=0 && s.charAt(i) == ' ')i--;//跳过单词间的空格
+            j=i;
+        }
+        return res.toString().trim();
+    }
+}

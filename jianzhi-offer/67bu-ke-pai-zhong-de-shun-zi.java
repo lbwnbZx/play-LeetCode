338a627133db098b26a8ed3c38e9b269edd4a3dc
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-05-12 09:56:50 +0800

    扑克牌的顺子

---
+++ jianzhi-offer/67bu-ke-pai-zhong-de-shun-zi.java
@@ -0,0 +1,12 @@
+﻿class Solution {
+    public boolean isStraight(int[] nums) {
+        int joker = 0;//记录0的个数
+        Arrays.sort(nums);
+        for(int i=0;i<4;i++){
+            if(nums[i]==0) joker++;
+            else if(nums[i]==nums[i+1]) return false;//有重复的牌则直接返回false
+        }
+        return nums[4]-nums[joker]<5;//最大牌-最小牌<5则构成顺子    
+
+    }
+}

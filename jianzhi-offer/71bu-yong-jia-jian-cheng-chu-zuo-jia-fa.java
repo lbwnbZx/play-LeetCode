4d40c770a393d35c9fb9374bddff291f00f36a84
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-05-19 17:15:54 +0800

    不用加减乘除做加法

---
+++ jianzhi-offer/71bu-yong-jia-jian-cheng-chu-zuo-jia-fa.java
@@ -0,0 +1,16 @@
+﻿/*
+位运算
+无进位和  和   异或运算  规律相同
+有进位和  和   与运算    规律相同（并且左移一位）
+
+*/
+class Solution {
+    public int add(int a, int b) {
+         while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}

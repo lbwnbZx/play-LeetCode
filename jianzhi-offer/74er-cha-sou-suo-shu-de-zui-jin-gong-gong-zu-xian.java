70bc17ae38e99a888734fee9033f07146c7e4ac1
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-06-02 10:03:17 +0800

    Add files via upload

---
+++ jianzhi-offer/74er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian.java
@@ -0,0 +1,39 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+ /*
+  //迭代
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null){
+            if(root.val < p.val && root.val < q.val) //pq都在root的右子树上
+                root = root.right;
+            else if(root.val > p.val && root.val > q.val) //pq都在root的左子树上    
+                root = root.left;
+            else  
+                break;    
+        }
+        return root;
+    }
+}
+
+ */
+
+//递归
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        if(root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        return root;
+    }
+}

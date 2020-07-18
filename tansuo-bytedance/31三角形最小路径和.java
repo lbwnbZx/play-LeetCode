8b13cca096c339e4e34a6fd51c1a19abcee3ccd8
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-07-18 10:05:05 +0800

    Create 31三角形最小路径和.java

---
+++ tansuo-bytedance/31三角形最小路径和.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n  = triangle.size();
+        int [][]dp = new int[n+1][n+1];
+        //从三角形最后一行开始递推
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j<= i ;j++){
+                dp[i][j] = Math.min(dp[i+1][j],dp[i+1][j+1])+triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+}

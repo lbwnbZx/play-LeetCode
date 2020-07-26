581e1d67258366f07e0ccb35a3fe2f604d25e978
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-07-26 11:09:30 +0800

    Update 10- I. 斐波那契数列.py

--- jianzhi-offer/python3/10- I. 斐波那契数列.py
+++ jianzhi-offer/python3/10- I. 斐波那契数列.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int) -> int:
         a,b = 0,1
         for _ in range(n): # 只做n次循环
-            a,b = b,a+b # 相当于java中申请一个零时变量temp交换a和b
+            a,b = b,a+b # 相当于java中申请一个临时变量temp交换a和b
         return a % 1000000007
 
         

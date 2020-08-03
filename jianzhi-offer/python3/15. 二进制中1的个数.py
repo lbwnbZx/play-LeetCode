88f63a1a0ee91e804faaf3196d3e3e72fd94d6b0
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-08-03 22:57:24 +0800

    Update 15. 二进制中1的个数.py

--- jianzhi-offer/python3/15. 二进制中1的个数.py
+++ jianzhi-offer/python3/15. 二进制中1的个数.py
@@ -1,16 +1,7 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 class Solution:
-    def deleteNode(self, head: ListNode, val: int) -> ListNode:
-        if head.val == val: return head.next
-        cur = head
-        while cur and cur.next:
-            if cur.next.val == val:
-                cur.next = cur.next.next
-            cur = cur.next
-
-        return head
-        
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n :
+            res += n & 1
+            n >>= 1 
+        return res

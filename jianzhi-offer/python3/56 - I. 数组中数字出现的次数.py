ed6578d39d11b214cdf9bde07e1275796be7d3b9
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-09-06 10:52:36 +0800

    Create 56 - I. 数组中数字出现的次数.py

---
+++ jianzhi-offer/python3/56 - I. 数组中数字出现的次数.py
@@ -0,0 +1,15 @@
+# 只出现一次的数字
+
+
+class Solution:
+    def singleNumbers(self, nums: List[int]) -> List[int]:
+        ret, a, b = 0, 0, 0 # ret 为所有数字异或的结果
+        for num in nums:
+            ret ^=num
+        h = 1
+        while (ret & h == 0):
+            h<<=1 # 找出ret中位数为1 的那一位
+        for num  in nums :
+            if(h & num == 0): a ^= num
+            else: b^=num
+        return [a, b]    

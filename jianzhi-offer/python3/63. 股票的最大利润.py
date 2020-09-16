6fd2f7f4d4203801b78a0a9e3d9a0ac0e509dc47
lbwnbZx <40679377+lbwnbZx@users.noreply.github.com>
2020-09-16 19:16:37 +0800

    Create 63. 股票的最大利润.py

---
+++ jianzhi-offer/python3/63. 股票的最大利润.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cost, profit = float("+inf"), 0  # cost 为最大值
+        for  price in prices:
+            cost = min(cost, price)
+            profit = max(profit, price - cost)
+        return profit    
